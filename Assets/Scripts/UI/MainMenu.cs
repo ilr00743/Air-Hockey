390ca0c476b3ace9440f0c798c84594137e1a7f7
illia <ilr00743@gmail.com>
2021-12-10 00:22:44 +0200

    Initial commit

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Level");
+    }
+}
