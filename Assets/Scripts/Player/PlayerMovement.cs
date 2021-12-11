5499036a81298af81fd5d47d0622a11a23c651ff
illia <ilr00743@gmail.com>
2021-12-11 15:36:32 +0200

    Initial commit

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,5 +5,22 @@ namespace Player
     public class PlayerMovement : MonoBehaviour
     {
         private Rigidbody2D _rigidbody;
+
+#if UNITY_EDITOR
+        private void Start()
+        {
+            Debug.Log("Editor");
+        }
+#endif
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        public void Move(Touch touch)
+        {
+            _rigidbody.position = new Vector2();
+            _rigidbody.MovePosition(_rigidbody.position);
+        }
     }
 }
